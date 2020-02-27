a95109d3b6999f72ce082062fcacc1e5ace1cc00
Badreddine8 <y8b8yop@gmail.com>
2020-02-27 11:00:27 +0100

    6

--- sarkpi.py
+++ sarkpi.py
@@ -3,4 +3,7 @@
 import os 
 
 
-print(os.system('./spark-submit --class org.apache.spark.examples.SparkPi --master spark://spark:7077 $SPARK_HOME/examples/jars/spark-examples*.jar 100'))
+str = os.system('./spark-submit --class org.apache.spark.examples.SparkPi --master spark://spark:7077 $SPARK_HOME/examples/jars/spark-examples*.jar 100')
+
+print(str)
+print("hello")
